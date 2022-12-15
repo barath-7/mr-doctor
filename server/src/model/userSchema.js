352ccc0695b004f88a247477141718ccb332e9cb
barath-7 <79738124+barath-7@users.noreply.github.com>
2022-12-15 23:37:35 +0530

    create user schema

---
+++ server/src/model/userSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({ 
+    name : {
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required: true,
+        trim: true
+    },
+    phoneNumber:{
+        type:String,
+        unique:true,
+        required: true,
+        trim: true
+    },
+    password:String, //TODO: Revistit password
+    dateOfBirth:Date,
+    address:String,
+    aadhaarNumber:String,
+    gender:{
+        type:String,
+        enum:[
+            "male",
+            "female"
+        ]
+    }
+ },
+ {
+    collection: 'user',
+    timestamps: true,
+}
+ );
+
+ const User = mongoose.model('User', userSchema);
+
+ module.exports = User;
