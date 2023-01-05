e0262bc200f33e01339def4d1df2b0f4b153061f
Krishna S <naveen.krishna.s@accenture.com>
2023-01-05 19:02:17 +0530

    MRDR:FE:Redux issues rectified

--- client/mr-doctor/src/store/store.js
+++ client/mr-doctor/src/store/store.js
@@ -14,6 +14,5 @@ export const store = configureStore({
     helperTextPassword: helperTextPasswordSlice,
     helperTextAddress: helperTextAddressSlice,
     helperTextDob: helperTextDobSlice,
-
   },
 });
